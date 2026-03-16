522d3f462eba5a42c84871df445d240e1533bbf7
rakibhassannayem <rakibhassannayem@gmail.com>
2026-03-16 11:34:04 +0600

    post events

---
+++ app/api/events/route.ts
@@ -0,0 +1,37 @@
+import connectDB from "@/lib/mongodb";
+import { NextRequest, NextResponse } from "next/server";
+import Event from "@/database/event.model";
+
+export async function POST(req: NextRequest) {
+  try {
+    await connectDB();
+
+    const formData = await req.formData();
+
+    let event;
+
+    try {
+      event = Object.fromEntries(formData.entries());
+    } catch (error) {
+      return NextResponse.json(
+        { message: "Invalid JSON data format", error },
+        { status: 400 },
+      );
+    }
+
+    const createdEvent = await Event.create(event);
+
+    return NextResponse.json(
+      {
+        message: "Event created successfully",
+        event: createdEvent,
+      },
+      { status: 201 },
+    );
+  } catch (e) {
+    return NextResponse.json({
+      message: "Event Creation Failed",
+      error: e instanceof Error ? e.message : "Unknown",
+    });
+  }
+}
